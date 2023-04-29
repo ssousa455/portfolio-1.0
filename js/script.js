7848cf42955b6fc06b2c94516553aea4e7a078bb
Sérgio Sousa <40366240+ssousa455@users.noreply.github.com>
2023-04-28 22:07:01 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+  const menuBtn = document.querySelector(".menu-btn");
+  const nav = document.querySelector("nav ul");
+
+  menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+
